55a23d86c41073575f25f1f98334bffd7c57f8d3
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-07-31 16:20:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,12 +9,3 @@ function doToElementsInArray(array, callback) {
 function changeCompletely(element, index, array) {
   array[index] = Math.floor(Math.random() * 100 + 2).toString() + ` ${array[index]}s!!!`;
 }
-
-
-// 3) index.js changeCompletely() is defined:
-//   ReferenceError: changeCompletely is not defined
-//     at Context.it (test/index-test.js:37:14)
-
-// 4) index.js changeCompletely() alters every element in an array when used in conjunction with Array.prototype.forEach():
-//   ReferenceError: changeCompletely is not defined
-//     at Context.it (test/index-test.js:43:21)
